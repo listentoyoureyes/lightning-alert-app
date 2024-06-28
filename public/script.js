18996081067b0d564e219086289de1d061fac8d5
lipovskoy <anton@volt.se>
2024-06-28 13:04:11 +0200

    changed script.js

--- public/script.js
+++ public/script.js
@@ -1,31 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const lightningList = document.getElementById('lightning-list');
-  
-    const fetchData = () => {
-      fetch('http://localhost:3001/lightning-data')
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          return response.json();
-        })
-        .then(data => {
-          lightningList.innerHTML = ''; // Clear the current list
-          // Sort data in descending order by number
-          data.sort((a, b) => b.number - a.number);
-          data.forEach(item => {
-            const listItem = document.createElement('li');
-            listItem.textContent = `#${item.number} - ${item.cities.join(', ')} - ${new Date(item.timestamp).toLocaleString()} - ${item.peakCurrent} kA`;
-            lightningList.appendChild(listItem);
-          });
-        })
-        .catch(error => {
-          console.error('Error fetching lightning data:', error);
+  const lightningList = document.getElementById('lightning-list');
+
+  const fetchData = () => {
+    fetch('/.netlify/functions/server/lightning-data')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        lightningList.innerHTML = ''; // Clear the current list
+        // Sort data in descending order by number
+        data.sort((a, b) => b.number - a.number);
+        data.forEach(item => {
+          const listItem = document.createElement('li');
+          listItem.textContent = `#${item.number} - ${item.cities.join(', ')} - ${new Date(item.timestamp).toLocaleString()} - ${item.peakCurrent} kA`;
+          lightningList.appendChild(listItem);
         });
-    };
-  
-    // Fetch data initially and then every 10 seconds
-    fetchData();
-    setInterval(fetchData, 10000);
-  });
-  
+      })
+      .catch(error => {
+        console.error('Error fetching lightning data:', error);
+      });
+  };
+
+  // Fetch data initially and then every 10 seconds
+  fetchData();
+  setInterval(fetchData, 10000);
+});
